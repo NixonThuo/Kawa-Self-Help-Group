9f2be4540ea3a9aded5bbe371c594a6549554568
Nixon Thuo <thuonixon808@gmail.com>
2017-06-22 10:51:41 +0300

    Welcome page edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Kawa Self Help Group
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Loans</a>
+                    <a href="#">Savings</a>
+                    <a href="#">Contributions</a>
+                    <a href="#">Projects</a>
+                    <a href="#">Funds</a>
                 </div>
             </div>
         </div>
